3f7f5257b671362b74b77f694bc327ce32b45766
ebrahimmuhammed107 <ebrahimmuhammed107@gmail.com>
2024-06-12 20:44:38 +0530

    Extent Content

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ const port=3000;
 const server=http.createServer((req,res)=>{
     res.statusCode=200;
     res.setHeader('content-Type','text/plain');
-    res.end('Assalamu Alaikkum \n');
+    res.end('Assalamu Alaikkum  Va Rahmathullahi vabarakathuhu\n');
 
 });
 server.listen(port,()=>{
